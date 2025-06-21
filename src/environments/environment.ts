560bd9a8ef243b9991d7f186576653664b4cbde5
Md Kamrul Islam <kislam@bunnings.com.au>
2025-06-21 20:14:56 +1000

    feat: removed credentials + progressbar styling

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,3 @@
 export const environment = {
-    firebase: {
-        apiKey: "AIzaSyAglob79XwBKlOK9oxAq718H6zhMXwNZVs",
-        authDomain: "nebula-prime-13736.firebaseapp.com",
-        projectId: "nebula-prime-13736",
-        storageBucket: "nebula-prime-13736.firebasestorage.app",
-        messagingSenderId: "857216678255",
-        appId: "1:857216678255:web:abd9c8c2466dd2bf53909a",
-        measurementId: "G-1Q2MXNLZSY"
-    }
+    firebase: {}
 }
